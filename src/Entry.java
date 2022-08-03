dc644f795e4f7f7c93883c6fcd0fcba763739cc8
HP <HP@LAPTOP-BKJDKAU9>
2022-08-03 20:10:56 +0530

    This My Cummit If you want to change it then say me

---
+++ src/Entry.java
@@ -0,0 +1,13 @@
+
+public class Entry {
+
+	String Name;
+	String Password;
+
+	public static void main(String[] args) {
+		
+		System.out.println("My name is Murli");
+		System.out.println("My name is Aniket he love one Girl her name is Neha");
+	}
+	
+}
